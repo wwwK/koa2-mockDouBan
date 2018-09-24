458c7db00439f90d9ffd35fe40ba2b6a9f10a3af
郝雷 <m13522389185@163.com>
2018-09-24 21:58:09 +0800

    feature: pug block includes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,20 @@ const Koa = require('koa');
 const app = new Koa();
 const { htmlTpl, ejsTpl, pugTpl } = require('./tpl');
 // const ejs = require('ejs');
-const pug = require('pug')
+const pug = require('pug');
+const views = require('koa-views');
+const { resolve } = require('path');
+
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+}))
+
+app.use(async (ctx, next) => {
+    await ctx.render('index', {
+        you: 'Luke',
+        me: 'HaoLLL'
+    })
+});
 
 // app.use(async (ctx, next) => {
 //     ctx.type = 'text/html;charset=utf-8'
@@ -12,11 +25,13 @@ const pug = require('pug')
 //     });
 // });
 
-app.use(async(ctx,next)=>{
-    ctx.type="text/html;charset=utf-8"
-    ctx.body = pug.render(pugTpl,{
-        you:'Luke',
-        me:'HaoLLL'
-    });
-});
+// app.use(async(ctx,next)=>{
+//     ctx.type="text/html;charset=utf-8"
+//     ctx.body = pug.render(pugTpl,{
+//         you:'Luke',
+//         me:'HaoLLL'
+//     });
+// });
+
+
 app.listen(4455);
